bc28426261630b943d6a216a9dbec09d4c7a9165
Henry Acosta <henry@mftech.io>
2024-03-09 19:13:53 -0500

    core: updates

--- lib/domain/usecases/sign_out.dart
+++ lib/domain/usecases/sign_out.dart
@@ -1,1 +1,14 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:password/core/usecase/use_case.dart';
+import 'package:password/domain/failures/failure.dart';
+import 'package:password/domain/repositories/authetication_repository.dart';
 
+class SignOut extends UseCase<void, any> {
+  SignOut(this.repository);
+
+  final AuthRepository repository;
+
+  Future<Either<Failure, void>> call() async {
+    await repository.signout();
+  }
+}
