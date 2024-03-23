aa5f4427affe962103794abfab48d4f2e6541e2e
Henry Acosta <henry@mftech.io>
2024-03-23 18:59:36 -0500

    core: updates

--- lib/core/utils/analytics.dart
+++ lib/core/utils/analytics.dart
@@ -22,4 +22,55 @@ class Analytics {
   Future<int?> getSessionId() {
     return _delegate.getSessionId();
   }
+
+  Future<void> logBeginCheckout({
+    double? value,
+    String? currency,
+    List<AnalyticsEventItem>? items,
+    String? coupon,
+    Map<String, Object?>? parameters,
+    AnalyticsCallOptions? callOptions,
+  }) async {
+    await _delegate.logBeginCheckout(
+      value: value,
+      currency: currency,
+      items: items,
+      coupon: coupon,
+      parameters: parameters,
+      callOptions: callOptions,
+    );
+  }
+
+  Future<void> logLogin({
+    String? loginMethod,
+    Map<String, Object?>? parameters,
+    AnalyticsCallOptions? callOptions,
+  }) async {
+    await _delegate.logLogin(
+      loginMethod: loginMethod,
+      parameters: parameters,
+      callOptions: callOptions,
+    );
+  }
+
+  Future<void> logScreenView({
+    String? screenClass,
+    String? screenName,
+    Map<String, Object?>? parameters,
+    AnalyticsCallOptions? callOptions,
+  }) async {
+    await _delegate.logScreenView(
+      screenClass: screenClass,
+      screenName: screenName,
+      parameters: parameters,
+      callOptions: callOptions,
+    );
+  }
+
+  Future<void> logAppOpen({
+    AnalyticsCallOptions? callOptions,
+    Map<String, Object?>? parameters,
+  }) async {
+    await _delegate.logAppOpen();
+  }
 }
